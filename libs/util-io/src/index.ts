4480f97a247f6cf8308165503b1f90a57cc2c919
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-11-18 11:53:13 -0500

    export functions

--- libs/util-io/src/index.ts
+++ libs/util-io/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export { processInput } from './lib/process-input';
+export { parseArray } from './lib/parse-array';
+export { readFile } from './lib/read-file';
+export { split } from './lib/split';
