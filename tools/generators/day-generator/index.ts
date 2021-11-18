02798a79af0d5fd0600c0df56310956cac46068b
Jackson Hardaker <jackson.hardaker@gmail.com>
2021-11-18 11:44:02 -0500

    initial generator template changes

--- tools/generators/day-generator/index.ts
+++ tools/generators/day-generator/index.ts
@@ -1,8 +1,35 @@
-import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
-import { libraryGenerator } from '@nrwl/workspace/generators';
+import {
+  Tree,
+  formatFiles,
+  installPackagesTask,
+  generateFiles,
+  joinPathFragments,
+  readProjectConfiguration,
+} from '@nrwl/devkit';
+import { applicationGenerator } from '@nrwl/node';
+import { paramCase } from 'change-case';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  const { name, func } = schema;
+  await applicationGenerator(tree, schema);
+
+  const libraryRoot = readProjectConfiguration(tree, name).root;
+  generateFiles(
+    tree, // the virtual file system
+    joinPathFragments(__dirname, './files'), // path to the file templates
+    libraryRoot, // destination path of the files
+    { name, func, tmpl: '', 'func-kebab': paramCase(func) } // config object to replace variable in file templates,
+  );
+
+  tree
+    .listChanges()
+    .filter(({ path }) => {
+      return path.includes('.gitkeep') || path.includes('environments');
+    })
+    .forEach(({ path }) => {
+      tree.delete(path);
+    });
+
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
